daf13a0a5cca29641955439c9ceeaa0e0233b863
deboramars <dbra.marques@gmail.com>
2018-11-11 19:27:29 -0300

    editado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import './App.css'
+import Inicio from './Inicio.js'
+import Profile from './Profile.js'
+import NewCard from './Newcard.js'
+import Follow from './Follow.js'
+import Card from './Card.js'
+import Trends from './Trends.js'
 
 class App extends Component {
 	render () {
 		return (
 			<div className="App">
-				<h1>Twitter Clone</h1>
+
+				<header className="App-Header">
+					<Inicio />
+				</header>
+
+				<header className="App-Profile">
+					<Profile />
+				</header>
+
+				<header className="App-Newcard">
+					<NewCard />
+				</header>
+
+				<header className="App-Follow">
+					<Follow />
+				</header>
+
+				<header className="App-Card">
+					<Card />
+				</header>
+
+				<header className="App-Trends">
+					<Trends />
+				</header>
+				
 			</div>
 		);
 	}
